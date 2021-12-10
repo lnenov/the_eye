5ea94c96333fd41cd27b328d6a6ef7e3f5e5cf8c
Lyudmil Nenov <lyudmil@toptal.com>
2021-12-10 12:14:35 +0200

    Install the event tracker django app

--- the_eye/settings.py
+++ the_eye/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'event_tracker.apps.EventTrackerConfig',
 ]
 
 MIDDLEWARE = [
