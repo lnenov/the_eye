96b1dfd555d13c65c4f0e1d3c15ed99e533cb690
Lyudmil Nenov <lyudmil@toptal.com>
2021-12-10 10:39:41 +0200

    Remove default sqlite database from project settings

--- the_eye/settings.py
+++ the_eye/settings.py
@@ -73,12 +73,7 @@ WSGI_APPLICATION = 'the_eye.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
