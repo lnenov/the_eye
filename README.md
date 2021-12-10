fd6ffac032569a43b8fbb23bf52d37593b963c34
Lyudmil Nenov <lyudmil@toptal.com>
2021-12-10 10:51:01 +0200

    Add python-dotenv to project
    This should allow for basic non-public configuraiton and easy local
    setup.

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ Local installation
 Install requirements with
 
     pip3 install --upgrade -r requirements.pip
+
+Edit .env file in the root directory of the project and add the following configuration entries:
+- SECRET_KEY
--- the_eye/settings.py
+++ the_eye/settings.py
@@ -10,22 +10,28 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(find_dotenv())
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9x7%bmb3*s2^=ri3-q%-qd52f=2aun0-g9oj1x4i+mch-d*xzz'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
 
 
 # Application definition
